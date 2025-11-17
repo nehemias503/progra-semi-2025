1e123810fb84b8f811bb4549929515601e057fd2
nehemias <neeemiasisai2007@gmail.com>
2025-11-17 11:30:18 -0600

    calculadora de cantidad

---
+++ MiprimerProyecto/MiprimerProyecto/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace MiprimerProyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboTipo = new System.Windows.Forms.ComboBox();
+            this.cboDe = new System.Windows.Forms.ComboBox();
+            this.cboA = new System.Windows.Forms.ComboBox();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.lblDe = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboTipo
+            // 
+            this.cboTipo.FormattingEnabled = true;
+            this.cboTipo.Items.AddRange(new object[] {
+            "Monedas",
+            "Longitud",
+            "Masa",
+            "Volumen",
+            "Almacenamiento",
+            "Tiempo",
+            "Area"});
+            this.cboTipo.Location = new System.Drawing.Point(181, 25);
+            this.cboTipo.Name = "cboTipo";
+            this.cboTipo.Size = new System.Drawing.Size(191, 21);
+            this.cboTipo.TabIndex = 0;
+            // 
+            // cboDe
+            // 
+            this.cboDe.FormattingEnabled = true;
+            this.cboDe.Location = new System.Drawing.Point(181, 75);
+            this.cboDe.Name = "cboDe";
+            this.cboDe.Size = new System.Drawing.Size(191, 21);
+            this.cboDe.TabIndex = 1;
+            // 
+            // cboA
+            // 
+            this.cboA.FormattingEnabled = true;
+            this.cboA.Location = new System.Drawing.Point(181, 133);
+            this.cboA.Name = "cboA";
+            this.cboA.Size = new System.Drawing.Size(191, 21);
+            this.cboA.TabIndex = 2;
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(116, 32);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(28, 13);
+            this.lblTipo.TabIndex = 3;
+            this.lblTipo.Text = "Tipo";
+            // 
+            // lblDe
+            // 
+            this.lblDe.AutoSize = true;
+            this.lblDe.Location = new System.Drawing.Point(116, 83);
+            this.lblDe.Name = "lblDe";
+            this.lblDe.Size = new System.Drawing.Size(21, 13);
+            this.lblDe.TabIndex = 4;
+            this.lblDe.Text = "De";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(122, 133);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(14, 13);
+            this.lblA.TabIndex = 5;
+            this.lblA.Text = "A";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(119, 198);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(48, 13);
+            this.lblCantidad.TabIndex = 6;
+            this.lblCantidad.Text = "cantidad";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(181, 198);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 7;
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Location = new System.Drawing.Point(121, 259);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(58, 13);
+            this.lblRespuesta.TabIndex = 8;
+            this.lblRespuesta.Text = "Respuesta";
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(363, 242);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(118, 46);
+            this.btnConvertir.TabIndex = 9;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 339);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.lblDe);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.cboA);
+            this.Controls.Add(this.cboDe);
+            this.Controls.Add(this.cboTipo);
+            this.Name = "Form1";
+            this.Text = "<";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboTipo;
+        private System.Windows.Forms.ComboBox cboDe;
+        private System.Windows.Forms.ComboBox cboA;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label lblDe;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.Button btnConvertir;
+    }
+}
+
